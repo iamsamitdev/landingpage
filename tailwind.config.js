99a2ef90b7c03f338cda470d8889619c1bbc35a2
Samit Koyom <samitkoyom@gmail.com>
2021-04-09 23:58:30 +0700

    final project

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans: ['Montserrat','Prompt','sans-serif']
+      }
+    },
   },
   variants: {
     extend: {},
